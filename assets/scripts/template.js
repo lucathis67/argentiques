686d1154ad715e35a1ac45f7afa423c06de5e9f0
lucathis67 <lucathis67@gmail.com>
2021-06-04 11:24:00 +0200

    integration of glightbox

--- assets/scripts/template.js
+++ assets/scripts/template.js
@@ -6,6 +6,7 @@ function main() {
 
 	//allow the opening of individual images on a full screen display
 	displayModal();
+
 }
 
 function copyright() {
@@ -17,64 +18,19 @@ function copyright() {
 	copyright.textContent = copyrightMessage;
 }
 
-function getTarget(e) {
-	if (!e) {
-		e = window.e;
-	}
-	return e.target || e.srcElement;
-}
-
 function displayModal() {
 
-	//creates the different element layers for the full screen views
-	let overlay = document.createElement('div');
-	overlay.setAttribute('id', 'overlay');
-	let frame = document.createElement('div');
-	frame.setAttribute('id', 'frame');
-	overlay.appendChild(frame);
-	let page = document.getElementsByTagName('body');
-	page[0].appendChild(overlay);
+	const lightbox = GLightbox();
+	lightbox.on('open', (target) => {
+	    console.log('lightbox opened');
+	});
+	const lightboxDescription = GLightbox({
+	    selector: '.glightbox2'
+	});
+	const lightboxVideo = GLightbox({
+	    selector: '.glightbox3'
+	});
 
-	function featureImage(e) {
-
-		document.getElementById('overlay').style.display = "block";
-		let featureImage = document.createElement('img');
-		featureImage.setAttribute('id', 'featured');
-		featureImage.src = getTarget(e).src;
-		frame.appendChild(featureImage);
-
-		//deactivates the full screen view when clicking anywhere
-		let overlayOff = function() {
-			document.getElementById('overlay').onclick = function() {
-				this.style.display = "none";
-				let featureImage = document.getElementById('featured');
-				frame.removeChild(featureImage);
-			}
-		}();
-	}
-
-	//activates the full screen view when clicking on an image
-	let overlayOn = function() {
-		let portfolio = document.getElementsByClassName('portfolio');
-		for (let i = 0; i < portfolio.length; i++) {
-			if (portfolio[i].addEventListener) {
-				portfolio[i].addEventListener('click', function(e) {
-					let target = getTarget(e);
-					if (target.hasAttribute('src')) {
-						featureImage(e);
-					}
-				}, false);
-			}
-			else {
-				portfolio[i].attachEvent('click', function(e) {
-					let target = getTarget(e);
-					if (target.hasAttribute('src')) {
-						featureImage(e);
-					}
-				});
-			}
-		}
-	}();
 }
 
 main();
